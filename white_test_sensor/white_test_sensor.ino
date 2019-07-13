c923b809cf6c7278dcc84d18333e59418b870190
Pitworker <sir.button.esq@gmail.com>
2019-07-12 22:53:43 -0400

    updated white_test_sensor to with an attempt at calculating velocity from the accel value. It doesn't work very well. Also mapped light brightness to gyro. Attempted to smooth out the brightening of the light in response to the motion, but work still needs to be done.

--- white_test_sensor/white_test_sensor.ino
+++ white_test_sensor/white_test_sensor.ino
@@ -7,35 +7,63 @@
 #define LED_PIN   6
 #define LED_COUNT 6
 
+#define DELAY_TIME 50
+
 // i2c
 Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
 
 // LED strip
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+float velocityX, 
+      velocityY,
+      velocityZ;
+
 void setupSensor() {
+  /*
+   * Initializes the LSM sensor
+   */
   if (!lsm.begin())
   {
     Serial.println("ERROR: Unable to initialize the LSM9DS1.");
     while (1);
   }
   Serial.println("LSM9DS1 found.");
-  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
+  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G);
   lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
   lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
 }
 
 void setupStrip() {
+  /*
+   * Initializes the LED strip
+   */
   strip.begin();
   strip.show();
   strip.setBrightness(50); // Sets the brightness to ~1/5 of max
 }
 
+float velocity(float prevV, float a) {
+  /*  
+   * Calculates the current velocity given the previous velocity
+   * and the current acceleration.
+   * NOTE: this ignores the presence of acceleration due to gravity,
+   *       so velocity will be inaccurate as a result, possibly by
+   *       a large factor.
+   */
+  float numSecs = ((float) DELAY_TIME) / 1000.0;
+  return (a * numSecs) + prevV; 
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   setupSensor();
   setupStrip();
+
+  velocityX = 0;
+  velocityY = 0;
+  velocityZ = 0;
 
   Serial.println("setup complete");
 }
@@ -48,37 +76,26 @@ void loop() {
   
   int accelCombined; 
 
-  int accelColor;
+  int rColor;
 
   lsm.read();
   lsm.getEvent(&a, &m, &g, &temp);
 
-  accelCombined = abs(int(a.acceleration.x) | 
-                      int(a.acceleration.y) | 
-                      int(a.acceleration.z));
-  
-  Serial.print("Accel X: "); 
-  Serial.print(a.acceleration.x); 
-  Serial.print(" m/s^2");
-  
-  Serial.print("\tY: "); 
-  Serial.print(a.acceleration.y);     
-  Serial.print(" m/s^2 ");
-  
-  Serial.print("\tZ: "); 
-  Serial.print(a.acceleration.z); 
-  Serial.println(" m/s^2 ");
+  //update velocity values
+  velocityX = velocity(velocityX, a.acceleration.x);
+  velocityY = velocity(velocityY, a.acceleration.y);
+  velocityZ = velocity(velocityZ, a.acceleration.z);
 
-  Serial.print("Combined accel: ");
-  Serial.println(accelCombined);
-
-  accelColor = map(min(accelCombined, 20), 0, 20, 0, 255);
+  
+  Serial.print("Gyro X: "); Serial.print(g.gyro.x);  Serial.print(" dps");
+  Serial.print("\tY: "); Serial.print(g.gyro.y);     Serial.print(" dps ");
+  Serial.print("\tZ: "); Serial.print(g.gyro.z);     Serial.println(" dps ");
+  
+  rColor = map(int(max(20.0, min(512.0, abs(g.gyro.x)))), 20, 512, 0, 255);
   
   for(int i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, strip.Color(int(a.acceleration.x * 10), 
-                           int(a.acceleration.y * 10),
-                           int(a.acceleration.z * 10)));
+    strip.setPixelColor(i, strip.Color(rColor, rColor, rColor));
   }
   strip.show();
-  delay(200);
+  delay(DELAY_TIME);
 }
